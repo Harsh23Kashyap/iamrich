5f33b3f448f654eef232cd4cda9cb34b861d17bd
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-02-13 11:56:04 +0530

    Body

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Center(
-    child: Text("Hello World"),
-  )));
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "I Am Rich",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Container(
+        color: Colors.blueGrey,
+        child: Center(
+            child: Image(
+          image: NetworkImage(
+              'https://imageio.forbes.com/specials-images/imageserve/60643a612c6235f001cc2f00/Diamond-close-up/960x0.jpg?fit=bounds&format=jpg&width=960'),
+        )),
+      ),
+    ),
+  ));
 }
